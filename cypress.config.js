7a21779fab2f200c9a30c5dfbffdef00cdc0211f
kkshinwari413 <kkshinwari413@gmail.com>
2023-12-15 15:25:20 +0500

    Run of specific browser

--- cypress.config.js
+++ cypress.config.js
@@ -1,24 +1,20 @@
 const { defineConfig } = require("cypress");
 
-
-
 module.exports = defineConfig({
-
   projectId: "ocnw76",
-  video:true,
+  video: true,
 
-
-  reporter: 'cypress-mochawesome-reporter',
+  reporter: "cypress-mochawesome-reporter",
 
   e2e: {
-    baseUrl: 'https://naveenautomationlabs.com/opencart/index.php?route=account/login',
+    baseUrl:
+      "https://naveenautomationlabs.com/opencart/index.php?route=account/login",
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      require('cypress-mochawesome-reporter/plugin')(on);
-
+      require("cypress-mochawesome-reporter/plugin")(on);
     },
   },
   env: {
-    URL: 'https://naveenautomationlabs.com/opencart/index.php?route=account/register'
-  }
+    URL: "https://naveenautomationlabs.com/opencart/index.php?route=account/register",
+  },
 });
